f9212d5de3f36bd8196c5914e4e6e61d6de09b18
torbatti <torbattii@gmail.com>
2024-08-05 10:37:59 +0330

    update: added khane fehrest links

---
+++ frontend/src/components/Khane/KhaneJadid.tsx
@@ -0,0 +1,15 @@
+import type { Component } from 'solid-js';
+import app_style from "./KhaneJadid.module.css"
+
+const KhaneJadid: Component = (props) => {
+  return (
+    <>
+      <div id="khane_fehrest">
+      
+
+      </div>
+    </>
+  );
+};
+
+export default KhaneJadid;
