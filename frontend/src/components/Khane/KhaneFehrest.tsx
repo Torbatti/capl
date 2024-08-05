6c4d869a16aef9761555cc5174483a24e99a6db1
torbatti <torbattii@gmail.com>
2024-08-05 17:18:39 +0330

    update: cool background gradient added

--- frontend/src/components/Khane/KhaneFehrest.tsx
+++ frontend/src/components/Khane/KhaneFehrest.tsx
@@ -4,24 +4,26 @@ import app_style from "./KhaneFehrest.module.css"
 const KhaneFehrest: Component = (props) => {
   return (
     <>
-      <div id="khane_fehrest" class={`${app_style.fehrest} box`}>
-        <a href="/" class={`${app_style.link}`}><div>
+      <div id="khane_fehrest" class={`${app_style.khane_fehrest}`}><div class={`${app_style.khane_fehrest__box} box`}>
+        <div class={`${app_style.khane_fehrest__bg}`}></div>
+        
+        <a href="/" class={`${app_style.khane_fehrest__link}`}><div>
           <p>مصورها</p>          
         </div></a>
-        <a href="/" class={`${app_style.link}`}><div>
+        <a href="/" class={`${app_style.khane_fehrest__link}`}><div>
           <p>نوشتارگان</p>          
         </div></a>
-        <a href="/" class={`${app_style.link}`}><div>
+        <a href="/" class={`${app_style.khane_fehrest__link}`}><div>
           <p>بازی ها</p>          
         </div></a>
-        <a href="/" class={`${app_style.link}`}><div>
+        <a href="/" class={`${app_style.khane_fehrest__link}`}><div>
           <p>نماواگان</p>          
         </div></a>
-        <a href="/" class={`${app_style.link}`}><div>
+        <a href="/" class={`${app_style.khane_fehrest__link}`}><div>
           <p>نواها</p>          
         </div></a>
 
-      </div>
+      </div></div>
     </>
   );
 };
