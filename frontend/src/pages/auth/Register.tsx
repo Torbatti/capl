32390d44c41b560711875bf254d54c07ec1a3b94
torbatti <torbattii@gmail.com>
2024-08-05 16:18:16 +0330

    update: clean ui rewamp

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,61 +1,56 @@
 import type { Component } from 'solid-js';
-import { createStore } from "solid-js/store";
-
 import app_style from "./Register.module.css"
 
 
-const EMAILS = ["johnsmith@outlook.com", "mary@gmail.com", "djacobs@move.org"];
-
-function fetchUserName(name) {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(EMAILS.indexOf(name) > -1), 200);
-  });
-}
+import auth_image from "./../../assets/images/reg_auth.jpg"
 
 const Register: Component = () => {
-  const [fields, setFields] = createStore();
-  const fn = (form) => {
-    // form.submit()
-    console.log("Done");
-  };
-
-  const userNameExists = async ({ value }) => {
-    const exists = await fetchUserName(value);
-    return exists && `${value} is already being used`;
-  };
-
-  const matchesPassword = ({ value }) =>
-    value === fields.password ? false : "Passwords must Match";
-
+  
 
   return (
     <>
       <div class={`${app_style.auth_box}`}>
-        <form class={`${app_style.auth_form}`}>
+        
+        <div class={`${app_style.auth_box__image}`}>
+          <img src={auth_image} alt="https://unsplash.com/@eugene_golovesov" />
+        </div>
 
-          <div>
-            <p>ایمیل</p>
-          </div>
-          <input type="text" />
-          
-          <div>
-            <p>نام کاربری</p>
-          </div>
-          <input type="text" />
-          
-          <div>
-            <p>رمز عبور</p>
-          </div>
-          <input type="text" />
+        <div class={`${app_style.auth_box__form}`}>
+          <form>
+            
+            <div>
+              <p>نام کاربری</p>
+            </div>
+            <input
+              name="username"
+              type="text" 
+              required
+            />
+            
+            <div>
+              <p>رمز عبور</p>
+            </div>
+            <input
+              name="password"
+              type="password"
+              required
+              minlength="8"
+            />
 
-          <div>
-            <p>تایید رمز عبور</p>
-          </div>
-          <input type="text" />
+            <div>
+              <p>تایید رمز عبور</p>
+            </div>
+            <input
+              name="confirmpassword"
+              type="password"
+              required
+            />
 
-          <button type="submit" class={`${app_style.auth_btn}`}>ثبت نام</button>
-        </form>
+            <button type="submit" class={`${app_style.auth_btn}`}>ثبت نام</button>
+          </form>
           <p class={app_style.auth_link}>ثبت نام کرده اید؟ <a href="/auth/register">بخش ورود</a></p>
+        </div>
+
       </div>
     </>
   );
